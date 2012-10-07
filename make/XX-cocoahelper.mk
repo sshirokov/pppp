1d46a3b23947735c2620b34c30723538396229cf
Yaroslav Shirokov <slava@github.com>
2012-10-07 04:56:22 -0700

    Cocoahelper builder

---
+++ make/XX-cocoahelper.mk
@@ -0,0 +1,3 @@
+cocoahelper: | quicklisp-test
+	@echo "=> Attempting build of :lispbuilder-sdl with cocoahelper compile handler."
+	$(LISP) $(NODEBUG) $(LISP_LOAD_ARG) make/cocoahelper.lisp $(LISP_EVAL_ARG) '(quit)'
---
+++ make/cocoahelper.lisp
@@ -0,0 +1,24 @@
+(ql:quickload :cffi)
+
+(let (tried)
+  (labels ((cocoahelper-p (c)
+             (and (typep c 'condition)
+                  (equalp '(:FRAMEWORK "cocoahelper")
+                          (assoc :framework (car (simple-condition-format-arguments c))))))
+
+           (handler (c)
+             (when (and (not tried) (cocoahelper-p c) (find-restart 'asdf:retry))
+               (let* ((ql-path (asdf:system-source-directory (asdf:find-system :quicklisp)))
+                      (ql-dir (pathname-directory ql-path))
+                      (ql-root (reverse (rest (reverse ql-dir))))
+                      (ql-str (namestring (make-pathname :directory ql-root)))
+                      (res (and (> (length ql-str) 0)
+                                (asdf:run-shell-command "find ~s -name 'cocoahelper' | xargs make -C" ql-str))))
+                 (if (not (equalp res 0))
+                   (format *error-output* ">> Compilation of cocoahelper failed, not invoking retry~%" res)
+                   (progn
+                     (setf tried t)
+                     (invoke-restart (find-restart 'asdf:retry))))))))
+
+    (handler-bind ((CFFI:LOAD-FOREIGN-LIBRARY-ERROR #'handler))
+      (ql:quickload :lispbuilder-sdl))))
