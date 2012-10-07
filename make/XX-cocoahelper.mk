19e37af8d578d4443e665b6100c8428a1296a98d
Yaroslav Shirokov <slava@github.com>
2012-10-07 05:00:38 -0700

    Make noise at the end of a good lispbuilder-sdl compile.

--- make/XX-cocoahelper.mk
+++ make/XX-cocoahelper.mk
@@ -1,3 +1,4 @@
 cocoahelper: | quicklisp-test
 	@echo "=> Attempting build of :lispbuilder-sdl with cocoahelper compile handler."
 	$(LISP) $(NODEBUG) $(LISP_LOAD_ARG) make/cocoahelper.lisp $(LISP_EVAL_ARG) '(quit)'
+	@echo "=> :lispbuilder-sdl built successfully!"
